df3e83503084734d3b719f6aae19e6ef0cc21ad7
ThunderKlappe <timschley@gmail.com>
2021-12-23 14:16:38 -0800

    Completed Assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...remove) {
+    for (let i = 0; i < array.length; i++){
+        for( let j = 0; j < remove.length; j++){
+            if (remove[j] === array[i]){
+                array.splice(i,1,"");
+            }  
+        }
+    }
+    array = array.filter(element => element);
 
-};
+    return array;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
