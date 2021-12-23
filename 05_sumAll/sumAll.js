89d9498bf977da54d92cca75dc13dd9af12ceaa3
ThunderKlappe <timschley@gmail.com>
2021-12-23 14:41:21 -0800

    Completed assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if ((firstNum < 0 || secondNum < 0) || 
+    (typeof firstNum != "number" || typeof secondNum != "number")){
+        return "ERROR";
+    }
+    if(firstNum > secondNum){
+        return sumAll(secondNum,firstNum);
+    }
+    let sum = 0;
+    for(let i = firstNum; i <= secondNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
