47138fbfb74a752900fd13abd4f52dee29b968eb
ThunderKlappe <timschley@gmail.com>
2021-12-22 20:55:55 -0800

    Completed Assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function(str) {
-
-};
+function reverseString(str) {
+    let reverse = "";
+    for (let i = str.length-1; i>=0; i--){
+        reverse += str.charAt(i);
+    }
+    return reverse;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
