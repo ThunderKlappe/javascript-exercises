c15aa9a216da6c116e5f926fd0dd8ab3d5015784
ThunderKlappe <timschley@gmail.com>
2022-01-05 15:30:23 -0800

    completed assignment 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    //turn input into an array
+    let array = Array.from(input);
+    //make an array with only the letters in lower case
+    let reduced = array.filter(char => char.toUpperCase() != char.toLowerCase());
+    reduced = reduced.map(ele => ele.toLowerCase());
+    //make a reverse array
+    let revArray = reduced.map(ele => ele);
+    revArray.reverse();
+    //compare arrays and return if true
+    return equals(reduced, revArray);
 
 };
+
+const equals = (a, b) =>
+  a.length === b.length &&
+  a.every((v, i) => v === b[i]);
 
 // Do not edit below this line
 module.exports = palindromes;
