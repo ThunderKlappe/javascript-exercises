4cb442136359222688929cf14cf0418fb08cb58c
ThunderKlappe <timschley@gmail.com>
2022-01-05 16:27:25 -0800

    completed assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+
+    if (input <0){
+        return "OOPS"
+    }
+
+    //set firstvariable to 1
+    let firstNo = 1;
+    //set secondvariable to 0
+    let secNo = 0;
+    //set fibnumber to 1
+    let fibNo = 1;
+    
+    //loop up to input
+    for(let i = 1; i < Number(input); i++){
+    //add first and second into fibno
+    fibNo = firstNo + secNo;
+    //set second to first
+    secNo = firstNo;
+    //set first to fibno
+    firstNo = fibNo;
+    }
+    return fibNo;
 
 };
 
