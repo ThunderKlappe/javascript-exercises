d8d31985857f913ec62bce10a41d8992d3c9fecf
ThunderKlappe <timschley@gmail.com>
2022-01-10 17:35:04 -0800

    Completed assignmnet 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((a,b) => {
+        if (!a.yearOfDeath){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!b.yearOfDeath){
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        let oldest;
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            oldest = a;
+        }else{
+            oldest = b;
+        }
+        return oldest;
+    }, {})
 
 };
 
